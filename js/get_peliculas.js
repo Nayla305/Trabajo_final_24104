d713c4d7f2a060b0ff6717fc69ad442847d62d3f
Nayla305 <164569960+Nayla305@users.noreply.github.com>
2024-07-09 19:02:32 -0300

    Add files via upload

---
+++ js/get_peliculas.js
@@ -0,0 +1,48 @@
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+    
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+   
+    const response = await fetch('http://localhost:8080/apimovies/peliculas', options);
+    const data = await response.json();
+    const movies = data;
+    const tbody = document.getElementById('bodyTablePeliculas');
+
+   
+    movies.forEach(movie => {
+        
+        const tr = document.createElement('tr');
+        const tdTitle = document.createElement('td');
+        tdTitle.textContent = movie.titulo;
+        const tdDuration = document.createElement('td');
+        tdDuration.textContent = movie.duracion;
+        const tdGenres = document.createElement('td');
+        tdGenres.textContent = movie.genero;
+        const tdImage = document.createElement('td');
+        const img = document.createElement('img');
+        img.src = "../assets/images/" + movie.imagen;
+        img.width = '150';
+        img.alt = movie.titulo;
+        tdImage.appendChild(img);
+        img.classList.add('img-fluid');
+        img.classList.add('img-thumbnail');
+        
+       
+        tr.appendChild(tdTitle);
+        tr.appendChild(tdDuration);
+        tr.appendChild(tdGenres);
+        tr.appendChild(tdImage);
+        
+        tbody.appendChild(tr);
+
+    });
+
+
+});
---
+++ js/post_pelicula.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', async () => {
+
+    formNuevaPelicula = document.getElementById('formNuevaPelicula');
+
+
+       formNuevaPelicula.addEventListener('submit', async (event) => {
+
+         event.preventDefault();
+
+        const formData = new FormData(formNuevaPelicula);
+
+     
+        const titulo = formData.get('titulo');
+        const genero = formData.get('genero');
+        const duracion = formData.get('duracion');
+        const imagen = formData.get('imagen');
+
+  
+        if (titulo === '' || genero === '' || duracion === '' || imagen === '') {
+            alert('Todos los campos son obligatorios');
+            return;
+        }
+
+  
+        const imageName = imagen.name;
+      
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                titulo: titulo,
+                genero: genero,
+                duracion: duracion,
+                imagen: imageName
+            })
+        };
+
+        const response = await fetch('http://localhost:8080/apimovies/peliculas', options);
+        const data = await response.json();
+
+
+        
+        if (response.status === 201) {
+            alert('Pelicula agregada correctamente');
+            formNuevaPelicula.reset()
+            location.reload();
+        } else {
+            alert('Error al agregar la pelicula');
+        }
+       
+    });
+
+});
